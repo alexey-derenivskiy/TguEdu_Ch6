a3c7478da1c03cabf9ea4d92b7b846de68c8963b
Aleksey Derenivskiy <dap_1988@mail.ru>
2021-11-30 21:18:00 +1200

    comlted

---
+++ app/src/main/java/com/dap/tguedu_ch6/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.dap.tguedu_ch6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
